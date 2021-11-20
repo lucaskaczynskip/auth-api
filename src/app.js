c75c44b3c5b7cb2fa390817a358966693a22f7ad
lucaskaczynskip <lucaskaczyn@gmail.com>
2021-11-20 15:46:58 -0300

    mongoDB connection

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan')
 const routes = require('./routes');
+
+require('./config/connection');
 
 class App {
   constructor() {
@@ -11,6 +14,7 @@ class App {
 
   middlewares() {
     this.app.use(express.json());
+    this.app.use(morgan('dev'));
 
     this.app.use((req, res, next) => {
       res.header("Acess-Controll-Allow-Origin", "*");
