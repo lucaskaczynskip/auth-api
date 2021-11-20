891690d2fc7a9b287184ecc7d973606ed1b20da5
lucaskaczynskip <lucaskaczyn@gmail.com>
2021-11-20 15:47:16 -0300

    get user route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
 
+// Controllers
+const UserController = require('./app/Controllers/UserController');
+
 const routes = new Router();
 
-
+routes.get('/user', UserController.show);
 
 module.exports = routes;
