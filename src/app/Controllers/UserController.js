975f2bbdf7d694bcb51bab20d5eef09eb3e23413
lucaskaczynskip <lucaskaczyn@gmail.com>
2021-11-20 17:24:53 -0300

    User store

--- src/app/Controllers/UserController.js
+++ src/app/Controllers/UserController.js
@@ -1,8 +1,60 @@
+const User = require('../Models/User');
+const yup = require('yup');
+const bcrypt = require('bcryptjs');
+
 class UserController {
   show(req, res) {
     res.status(200).json({
       message: 'Here'
     })
+  }
+
+  async store(req, res) {
+    let schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+      password: yup.string().required()
+    })
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: true,
+        message: "Invalid data."
+      })
+    }
+
+    const { name, email, password } = req.body;
+
+    const userAlreadyExists = await User.findOne({ email });
+
+    if (userAlreadyExists) {
+      return res.status(400).json({
+        error: true,
+        message: "User already exists."
+      })
+    }
+
+    const data = { name, email, password };
+    data.password = await bcrypt.hash(data.password, 8);
+
+    const user = User.create(data, (err) => {
+      if (err) {
+        return res.status(400).json({
+          error: true,
+          message: err
+        })
+      }
+
+      res.status(200).json({
+        error: false,
+        message: "Successfully registered user.",
+        user: {
+          ...data
+        }
+      })
+    })
+
+    return user;
   }
 }
 
