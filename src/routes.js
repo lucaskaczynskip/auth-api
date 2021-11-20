01ac607e61c813b554bf973ec921aa3eab5e62d5
lucaskaczynskip <lucaskaczyn@gmail.com>
2021-11-20 18:01:26 -0300

    token validate

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,12 @@ const { Router } = require('express');
 // Controllers
 const UserController = require('./app/Controllers/UserController');
 const LoginController = require('./app/Controllers/LoginController');
+const Authenticate = require('./app/Middlewares/Authenticate');
 
 const routes = new Router();
 
 // User routes
-routes.get('/user', UserController.show);
+routes.get('/user', Authenticate, UserController.show);
 routes.post('/user', UserController.store);
 
 // Login route
