804810e81836e722d184f91e6b60ab55b240c6d2
lucaskaczynskip <lucaskaczyn@gmail.com>
2021-11-20 17:25:03 -0300

    User post method

--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,6 @@ const UserController = require('./app/Controllers/UserController');
 const routes = new Router();
 
 routes.get('/user', UserController.show);
+routes.post('/user', UserController.store);
 
 module.exports = routes;
