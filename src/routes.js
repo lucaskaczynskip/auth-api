b3ab101f23bb2512a19f1ab13e49270f98ea6fc8
lucaskaczynskip <lucaskaczyn@gmail.com>
2021-11-20 17:49:51 -0300

    login route

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,15 @@ const { Router } = require('express');
 
 // Controllers
 const UserController = require('./app/Controllers/UserController');
+const LoginController = require('./app/Controllers/LoginController');
 
 const routes = new Router();
 
+// User routes
 routes.get('/user', UserController.show);
 routes.post('/user', UserController.store);
+
+// Login route
+routes.post('/login', LoginController.index);
 
 module.exports = routes;
