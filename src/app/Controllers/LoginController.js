ef097e9bda8dbbf47f2431ba2197d2206f1d2aff
lucaskaczynskip <lucaskaczyn@gmail.com>
2021-11-21 14:35:34 -0300

    Change json text

--- src/app/Controllers/LoginController.js
+++ src/app/Controllers/LoginController.js
@@ -11,7 +11,7 @@ class LoginController {
     if (!userExists) {
       return res.status(400).json({
         error: true,
-        message: "User already exists."
+        message: "User not exists."
       })
     }
 
